372e4275b7769c6fe8818b2129d7d481a45e4a3a
AgentGit <AgentGit@users.noreply.github.com>
2021-02-22 12:21:57 +0100

    Add Sketch file and App Icons. Experimenting with .onChange for scenePhase (WIP)

--- SimplePillTrackerWatchOnly WatchKit Extension/SimplePillTrackerWatchOnlyApp.swift
+++ SimplePillTrackerWatchOnly WatchKit Extension/SimplePillTrackerWatchOnlyApp.swift
@@ -9,11 +9,25 @@ import SwiftUI
 
 @main
 struct SimplePillTrackerWatchOnlyApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    
     @SceneBuilder var body: some Scene {
         WindowGroup {
             NavigationView {
                 MainView()
             }
+        }
+        .onChange(of: scenePhase) { newScenePhase in
+              switch newScenePhase {
+              case .active:
+                print("App is active")
+              case .inactive:
+                print("App is inactive")
+              case .background:
+                print("App is in background")
+              @unknown default:
+                print("Oh - interesting: I received an unexpected new value.")
+              }
         }
 
         WKNotificationScene(controller: NotificationController.self, category: "myCategory")
